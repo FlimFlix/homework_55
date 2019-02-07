6a3279cd7ee9642098803f2c97715f81d38ea383
Archer <arseniy.tarapon@gmail.com>
2019-02-07 18:36:12 +0600

    Добавил функцию, при которой со значением игрииента 0 нельзя убрать ингридиент.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Burger from './components/Burger/Burger';
 import BurgerForm from './components/BurgerForm/BurgerForm.js'
 
 
-// список доступных ингредиентов
 const availableIngredients = [
     {name: 'salad', price: 5, label: 'Салат'},
     {name: 'cheese', price: 20, label: 'Сыр'},
-    {name: 'meat', price: 30, label: 'Мясо'},
-    {name: 'bacon', price: 20, label: 'Бекон'}
+    {name: 'meat', price: 50, label: 'Мясо'},
+    {name: 'bacon', price: 30, label: 'Бекон'}
 ];
 
 
@@ -25,37 +24,24 @@ class App extends Component {
 
 
     addIngredient = (name) => {
-        // скопировать ингредиент
         let ingredient = {...this.state.ingredients[name]};
 
-        // поменять свойства в копии ингредиента
-        // find - метод массива, который работает аналогично findIndex,
-        // но находит не индекс элемента в массиве,
-        // а возвращает сам элемент.
         let price = availableIngredients.find(item => item.name === name).price;
         ingredient.count += 1;
         ingredient.total = ingredient.count * price;
 
-        // скопипровать объект "ингредиенты"
         let ingredients = {...this.state.ingredients};
 
-        // поменять ингредиент в копии объекта "ингредиенты"
         ingredients[name] = ingredient;
 
-        // скопировать состояние (state) компонента App.js
         let state = {...this.state};
 
-        // поменять объект "игредиенты" в копии состояния (state)
         state.ingredients = ingredients;
 
-        // задать новый state с перерисовкой компонентов на странице
         this.setState(state);
     };
 
     removeIngredient = (name) => {
-        // всё то же самое, что и в addIngredient
-        // только количество уменьшается на 1, а не увеличивается
-        // и есть проверка, что нельзя уменьшить количество ингредиента меньше 0
 
         let ingredient = {...this.state.ingredients[name]};
         let price = availableIngredients.find(item => item.name === name).price;
@@ -85,6 +71,12 @@ class App extends Component {
         return totalPrice;
     };
 
+    isDisabled = (ingredient) => {
+        console.log(ingredient);
+        return this.state.ingredients[ingredient].count === 0
+    };
+
+
     render() {
         return (
             <div className="App">
@@ -93,14 +85,10 @@ class App extends Component {
                     <BurgerForm getTotalPrice={this.getTotalPrice}
                                 clickRemoveIngredient={this.removeIngredient}
                                 clickAddIngredient={this.addIngredient}
-                                quantity={this.getQuantity}/>
+                                quantity={this.getQuantity}
+                                isDisabled={this.isDisabled}
+                    />
                 </div>
-                {/* здесь вывести панель с общей ценой */}
-                {/* для подсчёта цены можно добавить метод в App.js */}
-                {/* и вызвать его в фигурных скобках в JSX, */}
-                {/* чтобы получить и вывести результат. */}
-                {/* под ценой вывести форму BurgerForm */}
-                {/* в форме вывести IngredientControl для каждого ингредиента */}
             </div>
         );
     }
--- src/components/BurgerForm/IngredientControl.js
+++ src/components/BurgerForm/IngredientControl.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './IngredientControl.css';
 
 function IngredientControl(props) {
     return (
         <div className="ingredient_control">
             <p> <span>{props.label}: {props.ingredientQuantity()}</span>
-                <button type="submit" onClick={props.onRemoveIngredient}>Less</button>
+                <button type="submit" disabled={props.isDisabled} onClick={props.onRemoveIngredient}>Less</button>
                 <button type="submit" onClick={props.onAddIngredient}>More</button>
             </p>
         </div>
